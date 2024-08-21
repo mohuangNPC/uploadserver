d1b8071ccf5ab540562d51f8398ec903f24a1c17
mohuangNPC <272852115@qq.com>
2024-08-21 14:27:34 +0800

    init

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+使用该命令编译js，把依赖全部集成，否则会报错依赖找不到  
+ncc build index.js -o dist
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const core = require('@actions/core');
+const fs = require('fs');
+const axios = require('axios');
 
 try {
-    const name = core.getInput('name');
-    console.log(`Hello ${name}!`);
+    const upload_url = core.getInput('upload_url');
+    const upload_file = core.getInput('upload_file');
+    // 读取文件
+    const file = fs.createReadStream(upload_file);
+    const form = new FormData();
+    form.append('file', file);
+    axios.post(upload_url, form, {
+        headers: {
+            ...form.getHeaders()
+        }
+    }).then(response => {
+        console.log('File uploaded successfully:', response.data);
+        core.setOutput('result', 'File uploaded successfully');
+    }).catch(error => {
+        core.setFailed(`File upload failed: ${error.message}`);
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
