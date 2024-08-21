0f09c6e64176a73903b3cee6f6157cd62a1a0451
mohuangNPC <272852115@qq.com>
2024-08-21 14:35:57 +0800

    init

--- dist/index.js
+++ dist/index.js
@@ -33920,6 +33920,8 @@ const axios = __nccwpck_require__(9083);
 try {
     const upload_url = core.getInput('upload_url');
     const upload_file = core.getInput('upload_file');
+    console.log('upload_url', upload_url);
+    console.log('upload_file', upload_file);
     // 读取文件
     const file = fs.createReadStream(upload_file);
     const form = new FormData();
