9e5cfffbd8cf26994821922bc45111f6bb54a0e9
mohuangNPC <272852115@qq.com>
2024-08-21 14:34:54 +0800

    init

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 try {
     const upload_url = core.getInput('upload_url');
     const upload_file = core.getInput('upload_file');
+    console.log('upload_url', upload_url);
+    console.log('upload_file', upload_file);
     // 读取文件
     const file = fs.createReadStream(upload_file);
     const form = new FormData();
