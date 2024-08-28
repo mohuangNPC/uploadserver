8aab68809e61fe10d5745b0c3f3ea5f10b850bc2
mohuangNPC <272852115@qq.com>
2024-08-28 10:24:10 +0800

    增加信息文件

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const FormData = require('form-data');
 try {
     const upload_url = core.getInput('upload_url');
     const upload_file = core.getInput('upload_file');
+    // //信息文件，可选，包含这次上传文件的信息
+    const info_file = core.getInput('info_file');
+    // //可选，上传文件的鉴权token
+    const token = core.getInput('token');
     console.log('upload_url', upload_url);
     console.log('upload_file', upload_file);
     // 读取文件
     const file = fs.createReadStream(upload_file);
     const form = new FormData();
     form.append('file', file);
+    if(info_file !== undefined && info_file !== ''){
+        console.log("有信息文件")
+        form.append('info_file', fs.createReadStream(info_file));
+    }else{
+        console.log("没有信息文件")
+    }
     axios.post(upload_url, form, {
+        headers: {
+            'Authorization': token,
+        }
     }).then(response => {
         console.log('File uploaded successfully:', response.data);
         core.setOutput('result', 'File uploaded successfully');
